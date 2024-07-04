0e6037c7e3a910797934894e610156f12d485a59
Fairuz <ffairuz298@gmail.com>
2024-07-04 22:01:48 +0700

    Hasil Akhir

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*buat input nama user*/
 function replaceName() {
     let username = prompt("Siapakah namamu?", "");
     console.log("Username entered:", username);  // Debugging
@@ -10,6 +11,34 @@ function replaceName() {
 
 replaceName();
 
+/*buat hamburger menu*/
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navMenu = document.getElementById('nav-menu');
+
+    hamburger.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+        hamburger.classList.toggle('active');
+    });
+/*buat autoslider show*/
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+    let currentSlide = 0;
+
+    function showSlide(index) {
+        const slider = document.querySelector('.slider');
+        const transformValue = `translateX(-${index * 100}%)`;
+        slider.style.transform = transformValue;
+    }
+
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % totalSlides;
+        showSlide(currentSlide);
+    }
+
+    setInterval(nextSlide, 3000);
+});
+/*buat validasi form*/
 function validateForm() {
     const name = document.forms["message-form"]["full-name"].value;
     const birthDate = document.forms["message-form"]["birth-date"].value;
@@ -25,7 +54,7 @@ function validateForm() {
     
     return false
 }
-
+/*buat hasil validasi form*/
 function setSendurUI(name, birthDate, gender, messages) {
     document.getElementById("sender-full-name").innerHTML = name;
     document.getElementById("sender-birth-date").innerHTML = birthDate;
